cf58b25055251e9864533ba18dd4a9a3878fa2d6
Saraaevaa <asiria97@hotmail.com>
2026-09-18 19:38:25 +0200

    Crear estructura de datos

---
+++ datos.js
@@ -0,0 +1,13 @@
+const equipos = [
+    {
+        nombre: "Cadete B",
+        totalJugadores: 25,
+        jugadores: []
+    }
+];
+
+const rivales = [];
+
+const equipaciones = [];
+
+const cuerpoTecnico = [];
