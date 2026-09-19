18f6874d5db90e0f57a503f1d8d3886cdaeee8be
Saraaevaa <asiria97@hotmail.com>
2026-09-19 14:41:55 +0200

    Actualizar datos del equipo

--- datos.js
+++ datos.js
@@ -1,50 +1,149 @@
 const configuracion = {
-    convocadosOficial: 18
+    minimoConvocados: 1,
+    maximoConvocados: 99,
+    convocadosPorDefecto: 18,
+    convocadosOficial: 18,
+    campoIndependiente: true,
+    lugarIndependiente: true
 };
-
 
 const equipos = [
     {
         id: "cadete-b",
-        nombre: "Cadete B",
+        nombre: "Cadete B (Autonómica)",
         totalJugadores: 25,
 
         jugadores: [
-            {
-                dorsal: 1,
-                nombre: "Jugador de prueba 1",
-                posiciones: ["POR", "", ""]
-            },
-            {
-                dorsal: 2,
-                nombre: "Jugador de prueba 2",
-                posiciones: ["DEF", "MC", ""]
-            },
-            {
-                dorsal: 3,
-                nombre: "Jugador de prueba 3",
-                posiciones: ["DEF", "EXT", ""]
-            }
+            { dorsal: 1, nombre: "Brian Gómez", posiciones: ["Portero", "", ""] },
+            { dorsal: 13, nombre: "Felipe Barrios", posiciones: ["Portero", "", ""] },
+
+            { dorsal: 11, nombre: "Álvaro Rico", posiciones: ["Defensa", "", ""] },
+            { dorsal: 3, nombre: "Marcos Alonso", posiciones: ["Defensa", "", ""] },
+            { dorsal: 24, nombre: "Justin Chávez", posiciones: ["Defensa", "", ""] },
+            { dorsal: 4, nombre: "Álvaro Martí", posiciones: ["Defensa", "", ""] },
+            { dorsal: 5, nombre: "Rubén Molín", posiciones: ["Defensa", "", ""] },
+            { dorsal: 33, nombre: "Javier Galiña", posiciones: ["Defensa", "", ""] },
+            { dorsal: 12, nombre: "Álvaro Pírez", posiciones: ["Defensa", "", ""] },
+            { dorsal: 22, nombre: "Marcos López", posiciones: ["Defensa", "", ""] },
+
+            { dorsal: 8, nombre: "Ander Andoni", posiciones: ["Centrocampista", "", ""] },
+            { dorsal: 14, nombre: "Raúl Miguel", posiciones: ["Centrocampista", "", ""] },
+            { dorsal: 20, nombre: "Hugo Marcos", posiciones: ["Centrocampista", "", ""] },
+            { dorsal: 10, nombre: "Javier Fernández", posiciones: ["Centrocampista", "", ""] },
+            { dorsal: 16, nombre: "Juan Villanueva", posiciones: ["Centrocampista", "", ""] },
+            { dorsal: 15, nombre: "Daniel Gallo", posiciones: ["Centrocampista", "", ""] },
+            { dorsal: 19, nombre: "Eric Cámara", posiciones: ["Centrocampista", "", ""] },
+            { dorsal: 18, nombre: "Bruno Rossi", posiciones: ["Centrocampista", "", ""] },
+            { dorsal: 7, nombre: "Jaime Vera", posiciones: ["Centrocampista", "", ""] },
+            { dorsal: 21, nombre: "Diego Hernández", posiciones: ["Centrocampista", "", ""] },
+            { dorsal: 77, nombre: "Suren Militonian", posiciones: ["Centrocampista", "", ""] },
+
+            { dorsal: 17, nombre: "Jonathan Ezer", posiciones: ["Delantero", "", ""] },
+            { dorsal: 9, nombre: "Rodrigo Puerto", posiciones: ["Delantero", "", ""] }
         ],
 
-        entrenador: "Entrenador de prueba",
+        entrenador: "Eduardo Caballero",
 
         cuerpoTecnico: [
-            "Ayudante de prueba"
+            "David",
+            "Kim",
+            "Valenzuela"
         ]
     }
 ];
 
-
 const rivales = [
     {
         id: "fundacion-adf",
-        nombre: "Fundación ADF",
+        nombre: "Fundación ADF 'A'",
         escudo: "escudos/Fundación_ADF.png",
+        equipos: ["cadete-b"]
+    },
+    {
+        id: "juventud-sanse",
+        nombre: "Juventud Sanse 'A'",
+        escudo: "escudos/Juventud_Sanse.png",
+        equipos: ["cadete-b"]
+    },
+    {
+        id: "colmenar-viejo",
+        nombre: "A.D. Colmenar Viejo 'A'",
+        escudo: "escudos/Colmenar_Viejo.png",
+        equipos: ["cadete-b"]
+    },
+    {
+        id: "atletico-chopera",
+        nombre: "Atlético Chopera Alcobendas 04 'C'",
+        escudo: "escudos/Atlético_Chopera.png",
+        equipos: ["cadete-b"]
+    },
+    {
+        id: "academia-futbol-alcobendas",
+        nombre: "C.D.E. Academia de Fútbol Alcobendas 'A'",
+        escudo: "escudos/Academia_Fútbol_Alcobendas.png",
+        equipos: ["cadete-b"]
+    },
+    {
+        id: "rayo-ciudad-alcobendas",
+        nombre: "Rayo Ciudad Alcobendas C.F. 'B'",
+        escudo: "escudos/Rayo_Ciudad_Alcobendas.png",
+        equipos: ["cadete-b"]
+    },
+    {
+        id: "san-sebastian-reyes",
+        nombre: "U.D. San Sebastián de los Reyes S.A.D 'B'",
+        escudo: "escudos/San_Sebastián_Reyes.png",
+        equipos: ["cadete-b"]
+    },
+    {
+        id: "electrocor",
+        nombre: "C.D.Electrocor Las Rozas 'A'",
+        escudo: "escudos/Electrocor.png",
+        equipos: ["cadete-b"]
+    },
+    {
+        id: "dv7-madrid",
+        nombre: "CD DV7 Madrid 'A'",
+        escudo: "escudos/DV7_Madrid.png",
+        equipos: ["cadete-b"]
+    },
+    {
+        id: "union-adarve",
+        nombre: "A.D. Unión Adarve 'C'",
+        escudo: "escudos/Unión_Adarve.png",
+        equipos: ["cadete-b"]
+    },
+    {
+        id: "alcobendas",
+        nombre: "Alcobendas C.F. 'A'",
+        escudo: "escudos/Alcobendas.png",
+        equipos: ["cadete-b"]
+    },
+    {
+        id: "zona-norte",
+        nombre: "Unión Zona Norte 'A'",
+        escudo: "escudos/Zona_Norte.png",
+        equipos: ["cadete-b"]
+    },
+    {
+        id: "efmo-boadilla",
+        nombre: "E.F.M.O Boadilla 'A'",
+        escudo: "escudos/EFMO_Boadilla.png",
+        equipos: ["cadete-b"]
+    },
+    {
+        id: "galapagar",
+        nombre: "C.D. Galapagar 'B'",
+        escudo: "escudos/Galapagar.png",
+        equipos: ["cadete-b"]
+    },
+    {
+        id: "las-matas",
+        nombre: "U.D. Las Matas C.F. 'A'",
+        escudo: "escudos/Las_Matas.png",
         equipos: ["cadete-b"]
     }
 ];
-
 
 const equipaciones = [
     {
